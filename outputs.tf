c39a83e2e835296e1a8089d85e068fa4e938349c
David McPike <david.mcpike@clearesult.com>
2019-04-09 19:28:52 -0500

    add outbound IP addresses and managed identity info to outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,19 @@
-output "appService_default_url" {
+output "id" {
+  value = ["${azurerm_app_service.app.*.id}"]
+}
+
+output "app_service_default_url" {
   value = ["${azurerm_app_service.app.*.default_site_hostname}"]
+}
+
+output "outbound_ip_addresses" {
+  value = ["${azurerm_app_service.app.*.outbound_ip_addresses}"]
+}
+
+output "msi_principal_id" {
+  value = ["${azurerm_app_service.app.identity.0.principal_id}"]
+}
+
+output "msi_tenant_id" {
+  value = ["${azurerm_app_service.app.identity.0.tenant_id}"]
 }
