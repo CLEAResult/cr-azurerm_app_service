9eb735857f913e8906dd1769ae6a14422403ac3f
David McPike <david.mcpike@clearesult.com>
2019-07-24 22:01:40 -0500

    Forgot to commit slot.tf to slot support change.

---
+++ slot.tf
@@ -0,0 +1,46 @@
+resource "azurerm_app_service_slot" "app" {
+  name                = format("%s%03d-slot", local.name, count.index + 1)
+  count               = var.slot_num
+  location            = var.location
+  resource_group_name = var.rg_name
+  app_service_plan_id = var.plan
+  app_service_name    = azurerm_app_service.app[0].name
+  enabled             = "true"
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  app_settings = {
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE = "false"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      id,
+      app_service_plan_id,
+      tags,
+      app_settings,
+    ]
+  }
+
+  https_only = "true"
+
+  site_config {
+    always_on        = "true"
+    php_version      = var.win_php_version
+    linux_fx_version = format("%s%s", var.fx, var.fx_version)
+    http2_enabled    = var.http2_enabled
+    ftps_state       = var.ftps_state
+
+    default_documents = [
+      "index.html",
+      "index.php",
+    ]
+  }
+
+  tags = {
+    InfrastructureAsCode = "True"
+  }
+}
+
